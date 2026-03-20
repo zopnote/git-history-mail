a0ad3520c5f3e451104a6246d707e17749e39810
lenny <lennysiebert@proton.me>
2026-03-20 22:58:19 +0100

    Fixed wrong exe ext, added plan

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 
-> <img alt="img.png" src=".github/img.png" width="600"/>\
+> <img alt="img.png" src=".github/img.png" width="400"/>\
 > When changing your email address linked to your GitHub account,
 your profile commit history may not be shown anymore.
 With this script the email in your commit history of a
 repository will be overridden to match your new mail,
 linked with your GitHub account.
+
+I took this circumstance inside the cli-app to show off a tutorial to [stepflow](https://github.com/zopnote/stepflow).
 ## Usage
 1. Pull your repositories last commit.
 2. Ensure that you committed all of your changes and be on a clean working stage.
--- bin/git_mail.dart
+++ bin/git_mail.dart
@@ -3,7 +3,10 @@ import 'package:stepflow/io.dart';
 
 Future<void> main(List<String> raw) => runCommand(
   Command(
+    // STEPFLOW_TODO: Add use to format syntax as usage syntax.
+    // Add <argument>-usage to use syntax info.
     use: "git-mail",
+    // STEPFLOW_TODO: Add short description of cmds.
     description:
         "Tool to override the old mail inside a repository branch. "
         "Useful, when your account mail has changed and you're "
@@ -34,26 +37,31 @@ Future<void> main(List<String> raw) => runCommand(
       ),
     ],
     run: (info) async {
-      final String newMail = info.getFlag<String>("new_mail").value;
-      final String oldMail = info.getFlag<String>("old_mail").value;
-      final bool allBranches = info.getFlag<bool>("all_branches").value;
-      final bool includeTags = info.getFlag<bool>("include_tags").value;
-      final bool force = info.getFlag<bool>("force").value;
-      if (info.getFlag<bool>("help").value ||
-          newMail.isEmpty && oldMail.isEmpty)
-        return Response(info.formatSyntax());
+      final String newMail = info.getFlag("new_mail").value;
+      final String oldMail = info.getFlag("old_mail").value;
+      // STEPFLOW_TODO: Display implicit default value of bools in syntax message.
+      final bool allBranches = info.getFlag("all_branches").value;
+      final bool includeTags = info.getFlag("include_tags").value;
+      final bool force = info.getFlag("force").value;
+      // STEPFLOW_TODO: Remove the default help flag.
+      final bool help = info.getFlag("help").value;
 
-      if (newMail.isEmpty)
+      if (help || newMail.isEmpty && oldMail.isEmpty) {
+        // STEPFLOW_TODO: Add the formatSyntax padding.
+        return Response(info.formatSyntax());
+      }
+      if (newMail.isEmpty) {
         return Response(
           "Please specify a new mail with --new_mail.",
           Level.normal,
         );
-
-      if (oldMail.isEmpty)
+      }
+      if (oldMail.isEmpty) {
         return Response(
           "Please specify your old mail with --old_mail.",
           Level.normal,
         );
+      }
 
       return await runWorkflow(
         Chain(
@@ -61,6 +69,7 @@ Future<void> main(List<String> raw) => runCommand(
             Shell(
               program: "git",
               arguments: [
+                // STEPFLOW_TODO: Move Shell to app frame and argument constructor.
                 "filter-branch",
                 ..._get(force, "-f"),
                 "--env-filter",
@@ -81,6 +90,9 @@ Future<void> main(List<String> raw) => runCommand(
       );
     },
   ),
+  // STEPFLOW_TODO: move to named arguments in v3.
+  // The arguments from cli should be transferred implicitly.
+  // The option to override the implicit transferred arguments has to be given.
   raw,
 );
 
